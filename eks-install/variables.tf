82d2c705ec9a9c392443e1f1643d6c82a5f6dd86
Justin Conover <justin.conover@gmail.com>
2025-08-07 19:22:09 -0500

    adding eks build via terraform

---
+++ eks-install/variables.tf
@@ -0,0 +1,63 @@
+variable "region" {
+  description = "The AWS region to deploy the EKS cluster"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "The availability zones for the EKS cluster"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "The CIDR block for the private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "The CIDR block for the public subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  default     = "my-eks-cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "The version of the EKS cluster"
+  default     = "1.33"
+  type        = string
+}
+
+variable "node_groups" {
+  description = "EKS node group configuration"
+  type = map(object({
+    instance_types = list(string)
+    capacity_type  = string
+    scaling_config = object({
+      desired_size = number
+      max_size     = number
+      min_size     = number
+    })
+  }))
+  default = {
+    general = {
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+      scaling_config = {
+        desired_size = 2
+        max_size     = 4
+        min_size     = 1
+      }
+    }
+  }
+}
